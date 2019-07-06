19e1de969ce5467c42a35dfb6dee974b75bc409e
HugoBarrueta <hbarruetaf97@gmail.com>
2019-07-05 19:56:04 -0500

    Proyecto SummerKids

---
+++ SummerKidsPrueba/SummerKidsPrueba/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SummerKidsPrueba.Startup))]
+namespace SummerKidsPrueba
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
